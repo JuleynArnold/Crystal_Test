faa8a75389f1b822f7dbb8603ab18cdb394d9c89
Juleyn Arnold <juleyn.arnold@movehq.com>
2019-06-26 17:05:36 -0400

    fix: Experiment Branch
    Made changes to login controller so init is more clear

--- src/controller/login_controller.cr
+++ src/controller/login_controller.cr
@@ -3,11 +3,8 @@ require "../../src/controller/controller.cr"
 class LoginController < Controller
     def initialize(requestModule : String, body)
         #TODO: Eventually do something with the body
-        @content = Hash(String, String).new
-        @content["username"] = "LBL_USERNAME"
-        @content["password"] = "LBL_PASSWORD"
-        @content["actionRoute"] = "authenticate"
         @name = requestModule
+        @content = Hash{"username" => "LBL_USERNAME", "password" => "LBL_PASSWORD", "actionRoute" => "authenticate"}
     end
 
     def content

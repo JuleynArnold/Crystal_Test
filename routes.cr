bb090025c6640edb34bfae19aa456e1e3e58fe49
Juleyn Arnold <juleyn.arnold@movehq.com>
2019-09-19 16:30:28 -0400

    feat: Create User route Rearranged other routing naming convention Adds create user route so we can begin to store files Does preliminary work on updating db references

--- routes.cr
+++ routes.cr
@@ -7,8 +7,9 @@ require "./src/routing/route.cr"
 generateRouteClass("Login", "LoginController", "generateView", "./src/controller/login_controller.cr")
 
 #No auth
-generateRouteClass("GenerateSession", "Api", "login", "./src/api/api.cr")
+generateRouteClass("CreateUser", "CreateUserController", "createUser", "./src/controller/createuser_controller.cr")
+generateRouteClass("Authenticate", "Api", "authenticate", "./src/api/api.cr") #Returns a session id for the user
 
 #Required auth
-generateRouteClass("FilePost", "Api" , "updateServer", "./src/api/api.cr")
-generateRouteClass("Authenticate", "Api", "authenticate", "./src/api/api.cr")
+generateRouteClass("FilePost", "Api" , "updateServer", "./src/api/api.cr") #Adds files to the users individual directory
+generateRouteClass("Authorize", "Api", "authorize", "./src/api/api.cr") #Ensures that the sessionid is still valid and the action can
